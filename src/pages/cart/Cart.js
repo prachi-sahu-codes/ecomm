e4650921bdbd3d3d1888621a5a1eb228388e34ad
SahuPrachi <prachisahu998@gmail.com>
2023-05-25 23:50:14 +0530

    cart css added

--- src/pages/cart/Cart.js
+++ src/pages/cart/Cart.js
@@ -27,86 +27,95 @@ export const Cart = () => {
   };
 
   return (
-    <div className="div-padding">
-      <div className="page-content">
-        <div className="flex-center">
-          <h1 className="page-title">Cart</h1>
-          <span className="head-count">
-            ({count} Item{count > 1 ? "s" : ""})
-          </span>
+    <>
+      {cartData.length !== 0 ? (
+        <div className="div-padding">
+          <div className="page-content">
+            <div className="flex-center">
+              <h1 className="page-title">Cart</h1>
+              <span className="head-count">
+                ({count} Item{count > 1 ? "s" : ""})
+              </span>
+            </div>
+            <table className="cart-table">
+              <thead className="table-title">
+                <tr>
+                  <th className="thead-product">Product</th>
+                  <th>Quantity</th>
+                  <th className="thead-price">Total</th>
+                  <th className="thead-empty"></th>
+                </tr>
+              </thead>
+              <tbody className="cart-table-body">
+                {cartData?.map((item, index) => (
+                  <tr key={item._id}>
+                    <td>
+                      <div
+                        className={`cart-card-detail ${
+                          index === 0 ? "first-card" : ""
+                        }`}
+                        onClick={() => navigate(`/detail/${item._id}`)}
+                      >
+                        <img
+                          src={item?.image}
+                          className="cart-img"
+                          alt={item?.name}
+                        />
+                        <div className="cart-card-title-price">
+                          <p className="cart-item-name">{item?.name}</p>
+                          <p className="cart-item-catg">{item?.category}</p>
+                          <p className="cart-item-price">${item?.price}</p>
+                        </div>
+                      </div>
+                    </td>
+                    <td>
+                      <div className="cart-qty-price">
+                        <ProductQuantity _id={item?._id} qty={item?.qty} />
+                      </div>
+                    </td>
+                    <td className="cart-item-total-price">
+                      ${(item?.price * item?.qty).toFixed(2)}
+                    </td>
+                    <td className="cross-icon-btn">
+                      <BsXLg
+                        className="cross-icon"
+                        onClick={() => deleteCartItem(item._id)}
+                      />
+                      <button
+                        className="move-cart-btn"
+                        onClick={() => moveToWishlist(item)}
+                      >
+                        Move to Wishlist
+                      </button>
+                    </td>
+                  </tr>
+                ))}
+                <tr className="cart-final">
+                  <td></td>
+                  <td></td>
+                  <td>
+                    <p className="cart-final-price">Total: ${totalPrice}</p>
+                  </td>
+                  <td></td>
+                </tr>
+              </tbody>
+            </table>
+            <div className="check-btn-div">
+              <button
+                className="card-btn check-btn"
+                onClick={() => navigate(`/checkout`)}
+              >
+                Checkout
+              </button>
+            </div>
+          </div>
         </div>
-        <table className="cart-table">
-          <thead className="table-title">
-            <tr>
-              <th className="thead-product">Product</th>
-              <th>Quantity</th>
-              <th className="thead-price">Total</th>
-              <th className="thead-empty"></th>
-            </tr>
-          </thead>
-          <tbody className="cart-table-body">
-            {cartData?.map((item, index) => (
-              <tr key={item._id}>
-                <td>
-                  <div
-                    className={`cart-card-detail ${
-                      index === 0 ? "first-card" : ""
-                    }`}
-                    onClick={() => navigate(`/detail/${item._id}`)}
-                  >
-                    <img
-                      src={item?.image}
-                      className="cart-img"
-                      alt={item?.name}
-                    />
-                    <div className="cart-card-title-price">
-                      <p className="cart-item-name">{item?.name}</p>
-                      <p className="cart-item-catg">{item?.category}</p>
-                      <p className="cart-item-price">${item?.price}</p>
-                    </div>
-                  </div>
-                </td>
-                <td>
-                  <div className="cart-qty-price">
-                    <ProductQuantity _id={item?._id} qty={item?.qty} />
-                  </div>
-                </td>
-                <td className="cart-item-total-price">
-                  ${(item?.price * item?.qty).toFixed(2)}
-                </td>
-                <td className="cross-icon-btn">
-                  <BsXLg
-                    className="cross-icon"
-                    onClick={() => deleteCartItem(item._id)}
-                  />
-                  <button
-                    className="move-cart-btn"
-                    onClick={() => moveToWishlist(item)}
-                  >
-                    Move to Wishlist
-                  </button>
-                </td>
-              </tr>
-            ))}
-            <tr className="cart-final">
-              <td></td>
-              <td></td>
-              <td>
-                <p className="cart-final-price">Total: ${totalPrice}</p>
-              </td>
-              <td></td>
-            </tr>
-          </tbody>
-        </table>
-        <div className="check-btn-div">
-          <button
-            className="card-btn check-btn"
-            onClick={() => navigate(`/checkout`)}
-          >
-            Checkout
-          </button>
+      ) : (
+        <div className="no-data-page">
+          Discover the Art of Space: Your Empty Cart Awaits Your Perfect
+          Furniture Pieces!
         </div>
-      </div>
-    </div>
+      )}
+    </>
   );
 };
