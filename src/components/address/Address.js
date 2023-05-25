943e4d5ce708ff4fa78de33f4d237813159cc48e
SahuPrachi <prachisahu998@gmail.com>
2023-05-25 19:05:15 +0530

    default address added

--- src/components/address/Address.js
+++ src/components/address/Address.js
@@ -4,15 +4,50 @@ import "./address.css";
 import { AddressForm } from "./component/addressForm/AddressForm";
 import { FORM_ACTION_TYPE } from "../../reducer/actionType";
 import { useClick } from "../../context/ClickContext";
+import { useAuth } from "../../context/AuthContext";
 
 export const Address = ({ noDetail }) => {
   const { addressState, addressDispatch } = useClick();
+  const { loggedUser } = useAuth();
+  console.log(loggedUser?.address);
 
   return (
     <div>
       <h2 className="page-subhead">Shipping Information</h2>
 
       <ul>
+        {loggedUser?.address && (
+          <li
+            className="address-card"
+            onChange={() =>
+              addressDispatch({
+                type: FORM_ACTION_TYPE.SELECT_ADDRESS_FORM,
+                payload: "test-address",
+              })
+            }
+          >
+            {noDetail && (
+              <input id="test-address" type="radio" name="address-radio" />
+            )}
+            <label htmlFor="test-address">
+              <p className="address-card-head">
+                {loggedUser?.firstname} {loggedUser?.lastname}
+              </p>
+              <p className="address-card-subhead-details">
+                <span className="address-card-subhead">Address: </span>
+                {loggedUser?.address}
+              </p>
+              <p className="address-card-subhead-details">
+                <span className="address-card-subhead">Phone: </span>
+                1254
+              </p>
+              <p className="address-card-subhead-details">
+                <span className="address-card-subhead">Email: </span>
+                {loggedUser.email}
+              </p>
+            </label>
+          </li>
+        )}
         {addressState.addresses.map(
           ({
             _id,
@@ -66,7 +101,7 @@ export const Address = ({ noDetail }) => {
         }
       >
         <BsPlusLg className="new-address-icon " />
-        <span>Add new address</span>
+        <span>Add {loggedUser.address ? "new" : ""} address</span>
       </div>
 
       <div style={{ display: addressState.openedAddressForm ? "" : "none" }}>
