e0a21f0db3316f856dbd160b87aefafc5401ee84
SahuPrachi <prachisahu998@gmail.com>
2023-05-29 00:10:55 +0530

    filter positioning done for phone with icon

--- src/components/OnScrollHide.js
+++ src/components/OnScrollHide.js
@@ -2,10 +2,12 @@ import React, { useState, useEffect } from "react";
 import { useData } from "../context/ProductContext";
 import { ACTION_TYPE } from "../reducer/actionType";
 import { BsJustify } from "react-icons/bs";
+import { Filters } from "./Filters";
 
 export const OnScrollHide = () => {
   const { dispatch, state } = useData();
   const [isHidden, setIsHidden] = useState(false);
+  const [showFilter, setShowFilter] = useState(false);
 
   useEffect(() => {
     const handleScroll = () => {
@@ -27,26 +29,36 @@ export const OnScrollHide = () => {
       {isHidden ? (
         ""
       ) : (
-        <div className="flex-center listing-main-head">
-          <div className="burger-mb">
-            <BsJustify />
+        <div className="scroll-content">
+          <div className="flex-center listing-main-head">
+            <div className="burger-mb" onClick={() => setShowFilter((s) => !s)}>
+              <BsJustify />
+            </div>
+
+            <select
+              className="wrapper"
+              onChange={(e) =>
+                dispatch({
+                  type: ACTION_TYPE.SORT_SELECT,
+                  payload: e.target.value,
+                })
+              }
+              value={state.sortBy}
+            >
+              <option value="relevance">Relevent Products</option>
+              <option value="bestSell">Best Selling Products</option>
+              <option value="HTL">Price: High to Low</option>
+              <option value="LTH">Price: Low to High</option>
+            </select>
           </div>
 
-          <select
-            className="wrapper"
-            onChange={(e) =>
-              dispatch({
-                type: ACTION_TYPE.SORT_SELECT,
-                payload: e.target.value,
-              })
-            }
-            value={state.sortBy}
-          >
-            <option value="relevance">Relevent Products</option>
-            <option value="bestSell">Best Selling Products</option>
-            <option value="HTL">Price: High to Low</option>
-            <option value="LTH">Price: Low to High</option>
-          </select>
+          {showFilter && (
+            <div
+              className={`mb-filter-show ${showFilter ? "show-filter" : ""}`}
+            >
+              <Filters />
+            </div>
+          )}
         </div>
       )}
     </>
